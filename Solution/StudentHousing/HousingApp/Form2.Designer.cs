6feaa75007ee8c5782146642a6eeab5cc8ac3676
Okan Uzer <i463567@fhict.nl>
2022-12-12 12:49:28 +0100

    added inside signup form few things; - linklable for the terms & conditions - you have to agree the terms in order to click the submit button or else it will be locked. -added genders inside the signup, this will show up in the account profile later (consideration)

--- Solution/StudentHousing/HousingApp/Form2.Designer.cs
+++ Solution/StudentHousing/HousingApp/Form2.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -42,47 +41,44 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Fax", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(112, 115);
+            this.label1.Location = new System.Drawing.Point(116, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(259, 51);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome !";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label2.Location = new System.Drawing.Point(52, 219);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(380, 30);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Username (First name + Last name)";
-            // 
             // textBox1
             // 
             this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.textBox1.Location = new System.Drawing.Point(52, 252);
+            this.textBox1.Location = new System.Drawing.Point(52, 195);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(380, 31);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(52, 357);
+            this.textBox2.Location = new System.Drawing.Point(52, 367);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(380, 31);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(52, 457);
+            this.textBox3.Location = new System.Drawing.Point(52, 467);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(380, 31);
             this.textBox3.TabIndex = 2;
@@ -91,7 +87,7 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Location = new System.Drawing.Point(52, 306);
+            this.panel1.Location = new System.Drawing.Point(52, 245);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(380, 1);
             this.panel1.TabIndex = 3;
@@ -100,7 +96,7 @@
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel2.Location = new System.Drawing.Point(52, 411);
+            this.panel2.Location = new System.Drawing.Point(52, 421);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(380, 1);
             this.panel2.TabIndex = 3;
@@ -109,7 +105,7 @@
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel3.Location = new System.Drawing.Point(52, 529);
+            this.panel3.Location = new System.Drawing.Point(52, 539);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(380, 1);
             this.panel3.TabIndex = 3;
@@ -119,7 +115,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label3.Location = new System.Drawing.Point(52, 324);
+            this.label3.Location = new System.Drawing.Point(52, 334);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(112, 30);
             this.label3.TabIndex = 1;
@@ -130,7 +126,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label4.Location = new System.Drawing.Point(52, 424);
+            this.label4.Location = new System.Drawing.Point(52, 434);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(201, 30);
             this.label4.TabIndex = 1;
@@ -139,34 +135,37 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(299, 494);
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(61, 504);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(162, 29);
+            this.checkBox1.Size = new System.Drawing.Size(118, 25);
             this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Show Password";
+            this.checkBox1.Text = "I accept the ";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(52, 549);
+            this.button1.Location = new System.Drawing.Point(52, 649);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(380, 51);
             this.button1.TabIndex = 5;
-            this.button1.Text = "REGISTER";
+            this.button1.Text = "BACK TO LOGIN PAGE";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Enabled = false;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(52, 652);
+            this.button2.Location = new System.Drawing.Point(52, 560);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(380, 51);
             this.button2.TabIndex = 5;
-            this.button2.Text = "BACK TO LOGIN PAGE";
+            this.button2.Text = "SUBMIT";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // label5
@@ -178,29 +177,123 @@
             this.label5.TabIndex = 6;
             this.label5.Text = "If you already have an account / just made one, click on";
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(52, 288);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 29);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(192, 288);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(93, 29);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Location = new System.Drawing.Point(49, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(380, 30);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Username (First name + Last name)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(52, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gender";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Location = new System.Drawing.Point(52, 146);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(380, 1);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel5.Location = new System.Drawing.Point(52, 330);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(380, 1);
+            this.panel5.TabIndex = 3;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(339, 288);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(90, 29);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Others";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.linkLabel1.Location = new System.Drawing.Point(166, 504);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(251, 21);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Terms of Service and Privacy Policy";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(483, 712);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,7 +302,6 @@
         #endregion
 
         private Label label1;
-        private Label label2;
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
@@ -222,5 +314,13 @@
         private Button button1;
         private Button button2;
         private Label label5;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label6;
+        private Label label2;
+        private Panel panel4;
+        private Panel panel5;
+        private RadioButton radioButton3;
+        private LinkLabel linkLabel1;
     }
 }
--- Solution/StudentHousing/HousingApp/Form2.cs
+++ Solution/StudentHousing/HousingApp/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,27 @@ namespace HousingApp
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = checkBox1.Checked; 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Other than the content you own, under these Terms, STUDENTHOUSING and/or its licensors own all the intellectual property rights and materials contained in this application.\r\n\r\nYou are granted limited license only for purposes of viewing the material contained on this Website."); 
+            
         }
     }
 }
