8589181ca129f050f01848df871763e914d64508
Bruno Mella <507479@student.fontys.nl>
2022-12-14 11:07:49 +0200

    Grocery Class

---
+++ Solution/StudentHousing/HousingApp/Grocery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingApp
+{
+    public class Grocery
+    {
+        public string name;
+        public double price;
+
+        public Grocery(string name, double price) 
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public string GetName() 
+        { return name; }
+        public double GetPrice() 
+        { return price; }
+    }
+}
