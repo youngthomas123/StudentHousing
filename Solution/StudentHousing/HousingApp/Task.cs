2261e02d86edc6ae24337e2b33546d3ddddaf05e
Bruno Mella <507479@student.fontys.nl>
2022-12-14 10:59:35 +0200

    Task Class

--- Solution/StudentHousing/HousingApp/Task.cs
+++ Solution/StudentHousing/HousingApp/Task.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace HousingApp
 {
-    internal class Task
+    public class Task
     {
+        private string TaskName="";
+        private string TaskDescription="";
+        private string StartDate = "";
+        private string EndDate = "";
+        private bool Status; 
+        
+
+        public Task(string TaskName, string TaskDescription, string StartDate, string EndDate, bool Status) 
+        { 
+            this.TaskName = TaskName;
+            this.TaskDescription = TaskDescription;
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+            this.Status = Status;
+        }
+
+        public string GetTaskName() 
+        { return this.TaskName; }
+
+        public string GetTaskDescription() 
+        { return this.TaskDescription; }
+
+        public string GetStartDate() 
+        { return this.StartDate; }
+
+        public string GetEndDate() 
+        { return this.EndDate; }
+
+        public bool GetStatus() 
+        { return this.Status;}
     }
 }
